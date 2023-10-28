81d6317aa750fc4e9d5c4060bf06c4168f208a53
Vladimir <aqgz@email.cz>
2023-10-28 18:49:58 +0200

    commit

--- js/conditions.js
+++ js/conditions.js
@@ -1,10 +1,11 @@
 "use strict"
 
 const hamburger = 3;
-const fries = 0;
+const fries = 3;
 const cola = 0;
+const nuggets = 2;
 
-if(hamburger || cola || fries)
+if(hamburger === 3 && cola === 2 || fries === 3 && nuggets)
 {
     console.log('Все довольны!');
 }
