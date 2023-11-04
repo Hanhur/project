6abf7e344789c135f93a4cd0e5189c26b0f6c1b6
Vladimir <aqgz@email.cz>
2023-11-04 11:42:59 +0100

    commit

--- js/closureLexicalEnvironment.js
+++ js/closureLexicalEnvironment.js
@@ -30,4 +30,30 @@ const c1 = increment();
 const c2 = increment();
 const c3 = increment();
 
-console.log(c1, c2, c3);
+//console.log(c1, c2, c3);
+
+if (0) {
+    //console.log('Done!');
+}
+
+//console.log(typeof(+'4'));
+
+for (let i = 1; i < 5; i++) 
+{
+    //console.log(i + 1);
+}
+
+function foo(a,b) 
+{
+    const [first] = a;
+    const {eng} = b;
+ 
+    return `${first} ${eng}`;
+}
+ 
+const result = foo(['Hello', 'Привет'], {ru: 'Мир', eng: 'World'});
+//console.log(result);
+
+let time = '';
+5 > 3 || 2 ? time = 'Day' : time = 'Night' ;
+console.log(time);
